7e73029a19588110eac8dae8dd79eb30540e3c56
Kevin Edwards <kev@Macintosh-2.local>
2008-12-23 17:26:12 +0000

    rspec testing views correctly

--- init.rb
+++ init.rb
@@ -3,7 +3,8 @@ require 'sinatra'
 
 require File.expand_path(File.dirname(__FILE__) + '/lib/config')
 
+
+
 get '/' do
-  "Hello world"
-  #haml :index
+  haml :index
 end
--- test/init_spec.rb
+++ test/init_spec.rb
@@ -4,12 +4,21 @@ require 'sinatra'
 require 'sinatra/test/rspec'
 require 'init'
 
+PROJECT_ROOT = File.join(File.dirname(__FILE__), "..")
+set_options :views => File.join(PROJECT_ROOT, "views")
+
 describe 'My app' do
   
   it 'should show a default page' do
     get_it '/'
     @response.should be_ok
     @response.body.should == 'Hello world'
+  end
+  
+  it "should render index template" do
+    get_it '/index'
+    @response.should be_ok
+    #@response.should render('index')
   end
 
 end
