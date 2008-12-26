55f04105cc7327e417906da0d324523225f3723f
Kevin Edwards <kev@Macintosh-2.local>
2008-12-26 17:26:27 +0000

    retrieved user email

--- init.rb
+++ init.rb
@@ -16,7 +16,8 @@ require 'openid'
 require File.expand_path(File.dirname(__FILE__) + '/config/environment')
 
 require 'lib/active_record_store/active_record_store'
-
+require 'openid/extensions/sreg'
+require 'lib/models/user'
 
 include ActiveRecord
 
@@ -27,6 +28,8 @@ get '/' do
   haml :index
 end
 
+
+# begin open id authentication
 get '/submit_open_id' do
   
   ActiveRecord::Base.configurations = database_configuration
@@ -36,17 +39,41 @@ get '/submit_open_id' do
   open_id_store = ActiveRecordStore.new
   open_id_consumer = OpenID::Consumer.new(session, open_id_store)
   
-  #puts "open_id_store: #{open_id_store.class}"
   
-  #puts "Consumer: #{open_id_consumer.class}"
+
+
   
   check_id_request = open_id_consumer.begin("kevthedev.myopenid.com")
-  redirect(check_id_request.redirect_url("http://localhost:4567", "http://localhost:4567/authentication_complete"))
   
-  #{}"hello"
+  sregreq = OpenID::SReg::Request.new
+  #sregreq.request_fields User.sreg_required, true
+  #sregreq.request_fields User.sreg_optional, false
+  
+  sregreq.request_fields(["email"], true)
+  check_id_request.add_extension(sregreq)
+  
+  redirect(check_id_request.redirect_url("http://localhost:4567", "http://localhost:4567/authentication_complete"))
+
+
 end
 
+# end open id authentication
 get '/authentication_complete' do
-  "Authentication complete"
+  
+  open_id_store = ActiveRecordStore.new
+  open_id_consumer = OpenID::Consumer.new(session, open_id_store)
+  
+  oidresp = open_id_consumer.complete(session, "/authentication_complete")
+  # ...  
+ # if oidresp.status == OpenID::Consumer::SUCCESS
+    # ...  
+    user = User.new
+    user.email = params["openid.sreg.email"]
+    user.save
+    #user.assign_sreg_attributes! OpenID::SReg::Response.from_success_response(oidresp)
+    # ...  
+ # end
+  
+  "Authentication complete: #{params.to_yaml}"
 end
 
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,16 +1,16 @@
 require 'activerecord'
-#require File.join(File.dirname(__FILE__), '../../vendor/plugins/acts_as_simple_registration/lib/acts_as_simple_registration')
+require File.join(File.dirname(__FILE__), '../../vendor/plugins/acts_as_simple_registration/lib/acts_as_simple_registration')
 
 #File.join(File.dirname(__FILE__), 'database.yml')
 
 include ActiveRecord
-#include ActAsSimpleRegistration
+include ActAsSimpleRegistration
 
 class User < ActiveRecord::Base
   
-  # acts_as_simple_registration do
-  #     #required :email
-  #     #optional :nickname => :name
-  #   end
+  acts_as_simple_registration do
+    #required :email
+    #optional :nickname => :name
+  end
   
 end
