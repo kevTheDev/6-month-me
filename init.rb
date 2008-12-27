c1cf9beafe535c9789edfa348dc6f1efa1e67345
Kevin Edwards <kev@Macintosh-2.local>
2008-12-27 16:49:07 +0000

    fixed delete current user key/valeu from session after logout dont just set it to nil

--- init.rb
+++ init.rb
@@ -19,11 +19,7 @@ require 'lib/active_record_store/active_record_store'
 require 'openid/extensions/sreg'
 require 'lib/models/user'
 require 'lib/models/email'
-  require 'net/smtp'
 
-require 'lib/user_notifier'
-
-require 'action_mailer'
 require 'pony'
 
 include ActiveRecord
@@ -31,8 +27,23 @@ include ActiveRecord
 
 enable :sessions
 
+# like a before filter on all actions
+before do
+  connect_database
+  
+end
+
+get '/signin' do
+  haml :signin
+end
+
+
 
 get '/' do
+  # if logged_in?
+  #     redirect('new_email')
+  #     return
+  #   end
   haml :index
 end
 
@@ -40,9 +51,7 @@ end
 # begin open id authentication
 get '/submit_open_id' do
   
-  ActiveRecord::Base.configurations = database_configuration
-  ActiveRecord::Base.establish_connection(APP_ENV)
-  ActiveRecord::Base.logger = Logger.new("ar.log")
+  
   
   open_id_store = ActiveRecordStore.new
   open_id_consumer = OpenID::Consumer.new(session, open_id_store)
@@ -64,9 +73,12 @@ get '/authentication_complete' do
   open_id_store = ActiveRecordStore.new
   open_id_consumer = OpenID::Consumer.new(session, open_id_store)
   
+  logger = Logger.new("kev.log")
+  logger.info "SESSION KEYS: #{session.keys.to_yaml}"
+  
   oidresp = open_id_consumer.complete(session, "/authentication_complete")
 
-  logger = Logger.new("kev.log")
+  
   
   
   identity_url = params["openid.identity"]
@@ -77,10 +89,15 @@ get '/authentication_complete' do
   user.save
   
   session[:current_user] = user.id
-  haml :new_email
+  
+  redirect('new_email')
 end
 
 get '/new_email' do
+  haml :new_email
+end
+
+get '/create_email' do
   
   email = Email.new(:content => params[:email_content])
   email.user_id = session[:current_user]
@@ -91,11 +108,38 @@ get '/new_email' do
   
 
   # send the email
-  Pony.mail(:to => user.email, :from => 'admin@sixmonthsme.com', :subject => 'your six month reminder', :body => email.content)
-
-  
-  #UserNotifier.deliver_new_email(email)  
-  
+  Pony.mail(:to => user.email, :from => 'admin@sixmonthsme.com', :subject => 'your six month reminder', :body => email.content)  
   
 end
 
+get '/signin' do
+  redirect('/')
+end
+
+get '/signout' do
+  signout
+end
+
+def signout
+  #session[:current_user] = nil
+  session.delete(:current_user)
+  redirect('/')
+end
+
+def connect_database
+  ActiveRecord::Base.configurations = database_configuration
+  ActiveRecord::Base.establish_connection(APP_ENV)
+  ActiveRecord::Base.logger = Logger.new("ar.log")
+end
+
+def logged_in?
+  return false unless session
+  return false unless session[:current_user]
+  return true
+end
+
+def current_user
+  return nil unless logged_in?
+  @current_user = User.find(session[:current_user])
+  @current_user
+end
