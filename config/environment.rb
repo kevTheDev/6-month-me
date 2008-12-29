47a7574bdca360d4ecbb0d60ab0b9a18e81a1fc3
Kevin Edwards <kev@Macintosh-2.local>
2008-12-29 20:25:30 +0000

    fixed rake with env

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-#ENV['APP_ENV'] ||= 'development'
-#ENV['APP_ROOT'] ||= "#{File.dirname(__FILE__)}"
+ENV['APP_ENV'] ||= 'development'
+ENV['APP_ROOT'] ||= "#{File.dirname(__FILE__)}"
 
-APP_ENV = 'development'
-APP_ROOT = "#{File.dirname(__FILE__)}"
+APP_ENV = ENV['APP_ENV']
+APP_ROOT = ENV['APP_ROOT']
 
 def database_configuration_file
   File.join(File.dirname(__FILE__), 'database.yml')
