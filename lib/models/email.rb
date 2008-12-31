0299035adf91cc630da075c9f8aec7d5e7784cf1
Kevin Edwards <kev@Macintosh-2.local>
2008-12-31 16:54:44 +0000

    rake task to deliver scheduled emails

--- lib/models/email.rb
+++ lib/models/email.rb
@@ -1,4 +1,5 @@
 require 'activerecord'
+require 'pony'
 
 include ActiveRecord
 
@@ -17,15 +18,26 @@ class Email < ActiveRecord::Base
     user.email
   end
   
-  # all unsent emails that should have been sent before now
-  def self.scheduled_emails    
-    find(:all, :conditions => "sent_at IS NULL AND send_on < NOW()")
+  def self.deliver_scheduled_emails
+    scheduled_emails.each do |email|
+      email.deliver
+    end
   end
-  
+
+  def deliver
+    Pony.mail(:to => address, :from => 'noreply@sixmonthsme.com', :subject => 'A Letter From Your Past', :body => content)
+    update_attribute(:sent_at, DateTime.now)
+  end
+
   private
   
   def calculate_send_date
     update_attribute(:send_on, DateTime.now >> 6)
+  end
+  
+  # all unsent emails that should have been sent before now
+  def self.scheduled_emails    
+    find(:all, :conditions => "sent_at IS NULL AND send_on < NOW()")
   end
   
 end
--- lib/tasks/misc.rake
+++ lib/tasks/misc.rake
@@ -1,14 +1,13 @@
+require 'lib/models/email'
+require 'lib/models/user'
+
 task :default => :environment do
   # just run tests, nothing fancy
   Dir["test/**/*.rb"].sort.each { |test|  load test }
 end
 
 # sends any scheduled emails
-task :send_reminders => :environment do
-  
-  Email.scheduled_emails.each do |email|
-    Pony.mail(:to => email.address, :from => 'noreply@sixmonthsme.com', :subject => 'A Letter From Your Past', :body => email.content)
-  end
-  
+task :send_reminders => :environment do    
+  Email.deliver_scheduled_emails
 end
 
